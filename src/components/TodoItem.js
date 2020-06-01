41658895fb340f405fee45a2a15b254e54931c95
james-muriithi <muriithijames556@gmail.com>
2020-06-01 16:35:28 +0300

    added material ui

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 class TodoItem extends Component {
+
+    state = {
+        open: false
+    }
+
+    handleClickOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
-        const {id , title} = this.props.todo
+        const {id , title, end} = this.props.todo
         return (
-            <li> {title} 
+            <li> {title} {end}
                 <div className="buttons" >
                     <button className="remove" onClick={this.props.delTodo.bind(this,id)}>
                         <i className="fa fa-trash fs-19 text-danger"></i>
@@ -17,6 +38,46 @@ class TodoItem extends Component {
                     <button className="complete" onClick={this.handleClickOpen} >
                         <i className="fa fa-bell fs-19 text-primary"></i>
                     </button>
+                </div>
+                <div>
+                    <Dialog 
+                        fullWidth={true}
+                        maxWidth={'xs'}
+                    style={{ top: "0px!important", bottom: "0px!important"}}
+                    open={this.state.open} 
+                    onClose={this.handleClose} 
+                    aria-labelledby="form-dialog-title">
+                        <DialogTitle>
+                            Schedule a Reminder
+                        </DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Select the date and time
+                            </DialogContentText>
+                            <form noValidate>
+                                <TextField
+                                    id="datetime-local"
+                                    label="Reminder"
+                                    type="datetime-local"
+                                    margin="dense"
+                                    name="time"
+                                    fullWidth
+                                    
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                            </form>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={this.handleClose} color="primary">
+                                Cancel
+                        </Button>
+                            <Button onClick={this.handleClose} color="primary">
+                                Set Reminder
+                        </Button>
+                        </DialogActions>
+                    </Dialog>
                 </div>
             </li>
         )
